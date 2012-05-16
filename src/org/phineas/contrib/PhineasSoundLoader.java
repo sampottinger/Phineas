6d180b7f23a0d2ab29bfa7cacc41d5bd6e3e4089
A. Samuel Pottinger <Samnsparky@gmail.com>
2012-05-16 16:19:06 -0600

    Added stubs for PhineasSound and PhineasSoundLoader

---
+++ src/org/phineas/contrib/PhineasSoundLoader.java
@@ -0,0 +1,19 @@
+package org.phineas.contrib;
+
+import org.phineas.core.PhineasSound;
+
+// TODO: Singleton ... see PhineasSpriteLoader
+public class PhineasSoundLoader
+{
+	
+	public static PhineasSoundLoader getInstance()
+	{
+		throw new UnsupportedOperationException("Not yet implemented.");
+	}
+	
+	public PhineasSound loadSound(String loc)
+	{
+		throw new UnsupportedOperationException("Not yet implemented.");
+	}
+
+}
---
+++ src/org/phineas/core/PhineasSound.java
@@ -0,0 +1,23 @@
+package org.phineas.core;
+
+/**
+ * High level sound abstraction
+ * @author Sam Pottinger
+ */
+public class PhineasSound
+{
+	public void start()
+	{
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+	
+	public void stop()
+	{
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+	
+	public PhineasSound clone()
+	{
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+}
