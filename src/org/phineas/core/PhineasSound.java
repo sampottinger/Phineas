222bae6aacec0b5fab8a66aea9cf5da9a53e62f5
A. Samuel Pottinger <Samnsparky@gmail.com>
2012-05-17 10:24:13 -0600

    Updated PhineasSound interface

--- src/org/phineas/core/PhineasSound.java
+++ src/org/phineas/core/PhineasSound.java
@@ -6,6 +6,9 @@ package org.phineas.core;
  */
 public class PhineasSound
 {
+	
+	public enum VolumeLevel { LOW, LOW_MEDIUM, MEDIUM, HIGH_MEDIUM, HIGH };
+	
 	public void start()
 	{
 		throw new UnsupportedOperationException("Not yet implemented");
@@ -17,6 +20,32 @@ public class PhineasSound
 	}
 	
 	public PhineasSound clone()
+	{
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+	
+	/**
+	 * Sets the volume at which this sound will play. 
+	 * @param volumeLevel The new volume level at which this clip should play
+	 */
+	public void setVolume(VolumeLevel volumeLevel)
+	{
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+	
+	/**
+	 * Speeds up or slows down this clip
+	 * @param tempo Multiplier to use for the tempo (2 = twice as fast)
+	 */
+	public void setTempo(float tempo)
+	{
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+	
+	/**
+	 * Has this sound stop and go back to the start of the clip
+	 */
+	public void reset()
 	{
 		throw new UnsupportedOperationException("Not yet implemented");
 	}
